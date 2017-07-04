b020a27736bc3ae42108ac73f3fd6cbccb6ed9cb
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-07-04 14:06:04 +0000

    Added realize of method 'getOrderedByLastName' to UsersPresenter class

--- src/Task2/UsersPresenter.php
+++ src/Task2/UsersPresenter.php
@@ -4,6 +4,10 @@ namespace BinaryStudioAcademy\Task2;
 
 class UsersPresenter
 {
+    /**
+     * Repository instance
+     * @var Repository
+     */
     private $repository;
 
     public function __construct(Repository $repository)
@@ -11,9 +15,20 @@ class UsersPresenter
         $this->repository = $repository;
     }
 
+    /**
+     * Gets the sorted list of users from Repository
+     * by last name in ascending order.
+     *
+     * @return array The sorted data
+     */
     public function getOrderedByLastName(): array
     {
-        // TODO: implement with ascending sort
-        return [];
+        $users = $this->repository->getAll();
+
+        usort($users, function($a, $b) {
+            return $a['last_name'] <=> $b['last_name'];
+        });
+
+        return $users;
     }
 }
